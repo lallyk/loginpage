90e28e207ceb7d4fc42697798e8f4d134c386395
lalitha Kumari <alally371@gmail.com>
2023-04-09 01:29:30 +0530

    fixed typo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import LoginForm from './LoginForm';
+function App() {
+  
+  return (
+    <div className='main'>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
